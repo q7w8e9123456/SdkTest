f0306de032579565580a22863c6f7932f5c3397a
GAMANIA\chunhaoliu <chunhaoliu@gamania.com>
2021-04-12 16:02:59 +0800

    add lib and android maven setting

---
+++ firstlibrary/src/main/java/com/gamania/firstlibrary/SdkModel2.kt
@@ -0,0 +1,14 @@
+package com.gamania.firstlibrary
+
+class SdkModel2 {
+
+    private var version: Int = 0
+
+    fun setModelValue(ver: Int) {
+        version = ver
+    }
+
+    fun getModelValue():Int {
+        return version
+    }
+}
